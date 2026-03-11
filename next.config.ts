8fa28c02e979001c123f1c8ff15b0d6a4adca742
Frankemsinwa <frankemesinwa@gmail.com>
2026-03-11 03:32:00 +0100

    update

--- next.config.ts
+++ next.config.ts
@@ -15,6 +15,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'pics.avs.io',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
